6eaf3895e73267784e53cd432a8c4652e7abc916
Tamk1s <tamkis314@gmail.com>
2014-11-02 20:32:54 -0500

    Multiplayer Gameplay
    Did a (poor) attempt at multiplayer AI. The Army powerup enables an
    extra paddle with CPU AI for the colored player. Paddles seems to like
    converging towards each other towards the pucks simultaneously, and
    getting the puck stuck in each other. Also, guarding the goal is not
    working entirely (paddles are supposed to puppy guard their goal, and
    lock on to the puck's z position at the goal).
    
    Need to get proper collision working, probably without using our 6
    sphere collision hack, in order to fix these problems. I'll try
    replicating the behavior I used in the original 2D UAH game.

---
+++ Unity Hockey/Assets/Scripts/Gameplay/Core/PwrHUD.cs
@@ -0,0 +1,57 @@
+﻿//Behavior to attach/process physical goal objects
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TagFrenzy;
+using System;
+
+public class PwrHUD : MonoBehaviour
+{
+    public Toolbox.Color Color;
+    public Texture[] Tex = new Texture[19];   //The textures for each powerup
+    byte Pwr = 0;
+
+    void PowerupHUD()
+    {
+        bool Change = false;
+
+        if (Input.GetMouseButton(0) == true)
+        {
+            if (Pwr == 0)
+            {
+                Pwr = Toolbox.Consts.Num_Pwrs;
+            }
+            else
+            {
+                Pwr--;
+            }
+            Change = true;
+        }
+
+        if (Input.GetMouseButton(1) == true)
+        {
+            if (Pwr == Toolbox.Consts.Num_Pwrs)
+            {
+                Pwr = 0;
+            }
+            else
+            {
+                Pwr--;
+            }
+            Change = true;
+        }
+
+        if (Change == true)
+        {
+            //UpdateHUD();
+        }
+    }
+
+    void UpdateHUD(Toolbox.Color Clr, byte Index, byte[] sPwr)
+    {
+        Tags Color;
+        Color=Toolbox.Tagmgmt.ColorVal2Tag(Clr);
+        
+    }
+}
